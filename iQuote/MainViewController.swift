1b9b96426683e46964271547c57dbbf4a502d79b
SebaKrk <s.sciuba@gmail.com>
2021-11-17 21:59:26 +0100

    delete MainStoryBord and cre desVC in SceneDelegate

---
+++ iQuote/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  iQuote
+//
+//  Created by Sebastian Sciuba on 17/11/2021.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    private func setupView() {
+        view.backgroundColor = .red
+    }
+}
