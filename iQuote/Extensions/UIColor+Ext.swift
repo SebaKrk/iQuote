0188a2332209dab90d04fe8553872413a6de79b1
SebaKrk <s.sciuba@gmail.com>
2021-11-18 07:03:38 +0100

    create extension - UIColor , created main colors for the app

---
+++ iQuote/Extensions/UIColor+Ext.swift
@@ -0,0 +1,21 @@
+//
+//  UIColor+Ext.swift
+//  iQuote
+//
+//  Created by Sebastian Sciuba on 18/11/2021.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat , blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    static func primaryOrange() -> UIColor {
+        return rgb(red: 248, green: 165, blue: 42)
+    }
+    static func darkGray() -> UIColor {
+        return rgb(red: 37, green: 37, blue: 37)
+    }
+}
