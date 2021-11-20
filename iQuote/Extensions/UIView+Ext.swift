202f8fa46d109170eb63bc6a51a82da4f3e14728
SebaKrk <s.sciuba@gmail.com>
2021-11-20 08:52:47 +0100

    create extension UIView - setGradien func

---
+++ iQuote/Extensions/UIView+Ext.swift
@@ -0,0 +1,19 @@
+//
+//  UIView+Ext.swift
+//  iQuote
+//
+//  Created by Sebastian Sciuba on 18/11/2021.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    public func setGradien(colorOne: UIColor, colorTwo: UIColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors =  [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.5, 1.0]
+        layer.insertSublayer(gradientLayer, at: 1)
+    }
+}
