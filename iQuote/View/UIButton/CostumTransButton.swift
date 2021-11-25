62a577bcba0455f3ec1619f0a9bc8ebc2b0c4f3c
SebaKrk <s.sciuba@gmail.com>
2021-11-25 18:21:34 +0100

    create CostumTransButton() for expandable buttons

---
+++ iQuote/View/UIButton/CostumTransButton.swift
@@ -0,0 +1,59 @@
+//
+//  CostumTransButton.swift
+//  iQuote
+//
+//  Created by Sebastian Sciuba on 25/11/2021.
+//
+
+import Foundation
+import UIKit
+
+class CostumTransButton : UIButton {
+    
+    var buttonIsOn = false
+    var offButton = UIImage()
+    var onButton = UIImage()
+    let buttonSize: CGFloat = 50
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    convenience init(imageOne : String, imageTwo: String) {
+        self.init(frame: .zero)
+        offButton = UIImage(named: imageOne)!
+        onButton = UIImage(named: imageTwo)!
+        setImage(offButton, for: .normal)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+   
+        layer.masksToBounds = true
+        widthAnchor.constraint(equalToConstant: buttonSize).isActive = true
+        heightAnchor.constraint(equalToConstant: buttonSize).isActive = true
+        animate()
+    }
+    public func  flipLikeState() {
+        buttonIsOn = !buttonIsOn
+        animate()
+    }
+    private func animate() {
+        UIView.animate(withDuration: 0.1, animations: {
+            let newImg = self.buttonIsOn ? self.onButton : self.offButton
+            self.scaleAnimation()
+            self.setImage(newImg, for: .normal)
+        }, completion: { _ in
+            UIView.animate(withDuration: 0.3, animations: {
+                self.transform = CGAffineTransform.identity
+            })
+        })
+    }
+    private func scaleAnimation() {
+        transform = CGAffineTransform(scaleX: 2, y: 2)
+    }
+}
--- iQuote/View/ViewControllers/NavigationViewController.swift
+++ iQuote/View/ViewControllers/NavigationViewController.swift
@@ -10,15 +10,55 @@ import UIKit
 
 class NavigationViewController : UIViewController {
     
+    let quoteMenuContainer = UIView()
+    
     var mainQuoteButton = CostumeQuoteButton()
+    
+    var heartButton = CostumTransButton(imageOne: "heartButton", imageTwo: "heartButton2")
+    var listButton = CostumTransButton(imageOne: "listButton", imageTwo: "listButton2")
+    var nextButton = CostumTransButton(imageOne: "nextButton", imageTwo: "nextButton2")
+    var exportButton = CostumTransButton(imageOne: "exportButton", imageTwo: "exportButton2")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        configureQuoteMenuContainer()
         configureMainButton()
+        configureMenuButtons()
     }
+//    MARK: - OBJC Func
+    
     @objc func handleMainButton() {
         mainQuoteButton.flipLikeState()
+    }
+    @objc func handleHeartButton() {
+        heartButton.flipLikeState()
+    }
+    
+    @objc func handleListButton() {
+        listButton.flipLikeState()
+    }
+    
+    @objc func handleExportButton() {
+        exportButton.flipLikeState()
+    }
+    
+    @objc func handleNextButton() {
+        nextButton.flipLikeState()
+    }
+    
+//    MARK: - Constraints
+    
+    private func configureQuoteMenuContainer(){
+        view.addSubview(quoteMenuContainer)
+        quoteMenuContainer.translatesAutoresizingMaskIntoConstraints = false
+//        quoteMenuContainer.backgroundColor = .white
+
+        NSLayoutConstraint.activate([
+            quoteMenuContainer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            quoteMenuContainer.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            quoteMenuContainer.widthAnchor.constraint(equalToConstant: 200),
+            quoteMenuContainer.heightAnchor.constraint(equalToConstant: 200)
+        ])
     }
     
     private func configureMainButton() {
@@ -29,6 +69,33 @@ class NavigationViewController : UIViewController {
         NSLayoutConstraint.activate([
             mainQuoteButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             mainQuoteButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
+
+    private func configureMenuButtons() {
+        quoteMenuContainer.addSubview(heartButton)
+        quoteMenuContainer.addSubview(nextButton)
+        quoteMenuContainer.addSubview(exportButton)
+        quoteMenuContainer.addSubview(listButton)
+        
+        listButton.addTarget(self, action: #selector(handleListButton), for: .touchUpInside)
+        heartButton.addTarget(self, action: #selector(handleHeartButton), for: .touchUpInside)
+        exportButton.addTarget(self, action: #selector(handleExportButton), for: .touchUpInside)
+        nextButton.addTarget(self, action: #selector(handleNextButton), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            
+            nextButton.leadingAnchor.constraint(equalTo: mainQuoteButton.trailingAnchor,constant: 10),
+            nextButton.centerYAnchor.constraint(equalTo: mainQuoteButton.centerYAnchor),
+            
+            exportButton.trailingAnchor.constraint(equalTo: mainQuoteButton.leadingAnchor,constant: -10),
+            exportButton.centerYAnchor.constraint(equalTo: mainQuoteButton.centerYAnchor),
+            
+            heartButton.bottomAnchor.constraint(equalTo: mainQuoteButton.topAnchor,constant: -10),
+            heartButton.centerXAnchor.constraint(equalTo: mainQuoteButton.centerXAnchor),
+            
+            listButton.topAnchor.constraint(equalTo: mainQuoteButton.bottomAnchor,constant: 10),
+            listButton.centerXAnchor.constraint(equalTo: mainQuoteButton.centerXAnchor)
         ])
     }
     
