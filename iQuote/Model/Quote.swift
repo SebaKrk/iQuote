98c8b370415033b0c6a7cdc2c8356a79f4ecda8e
SebaKrk <s.sciuba@gmail.com>
2021-11-28 09:47:14 +0100

    create Quote struct and ZenQuoteError renum for network manager

---
+++ iQuote/Model/Quote.swift
@@ -0,0 +1,19 @@
+//
+//  Quote.swift
+//  iQuote
+//
+//  Created by Sebastian Sciuba on 28/11/2021.
+//
+
+import Foundation
+
+
+struct Quote : Codable,Hashable {
+    let q : String
+    let a : String
+}
+
+
+//q = quote text
+//a = author name
+//https://premium.zenquotes.io/zenquotes-documentation/#api-structure
---
+++ iQuote/Utilitis/ErrorMasasge.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMasasge.swift
+//  iQuote
+//
+//  Created by Sebastian Sciuba on 28/11/2021.
+//
+
+import Foundation
+
+enum ZenQuoteError : String,Error {
+    
+    case unableToCompleted = "Unable to completed your request. Please check your internet connection"
+    case inavlidResponse = "Invalid response from server. Please try again."
+    case incalidData = "The data received from the server was invalid. Please try again."
+}
