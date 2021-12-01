1a450ee81dba3831b59abdb8873f67307d527880
SebaKrk <s.sciuba@gmail.com>
2021-12-01 08:12:31 +0100

    create PersistenceManager - UserDefaults  for saving quote data

---
+++ iQuote/Manager/PersistenceManager.swift
@@ -0,0 +1,70 @@
+//
+//  PersistenceManager.swift
+//  iQuote
+//
+//  Created by Sebastian Sciuba on 01/12/2021.
+//
+
+import Foundation
+
+enum PersistenceAcatinType {
+    case add, remove
+}
+enum PersistenceManager {
+    
+    static private let defaults = UserDefaults.standard
+    
+    enum Keys {
+        static let favorites = "favorites"
+    }
+    
+    static func uppdateWith(favorite: Quote, actionType: PersistenceAcatinType , completed: @escaping(ZenQuoteError?)->Void) {
+        retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                var retrivedFavorites = favorites
+                
+                switch actionType {
+                case .add:
+                    guard  !retrivedFavorites.contains(favorite) else {
+                        completed(.alleredyInFavorites)
+                        return
+                    }
+                    retrivedFavorites.append(favorite)
+                case .remove:
+                    retrivedFavorites.removeAll {$0.q == favorite.q }
+                }
+                completed(save(favorites: retrivedFavorites))
+            case .failure(let error):
+                completed(error)
+            }
+        }
+    }
+    
+    
+    static func retrieveFavorites(completed: @escaping (Result<[Quote],ZenQuoteError>) -> Void) {
+        guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else {
+            completed(.success([]))
+            return
+        }
+        do {
+            let decoder = JSONDecoder()
+            let favorites = try decoder.decode([Quote].self, from: favoritesData)
+            completed(.success(favorites))
+            
+        } catch {
+            completed(.failure(.unableToFavorite))
+        }
+    }
+    
+    static func save(favorites: [Quote]) -> ZenQuoteError? {
+        do {
+            let encoder = JSONEncoder()
+            let encoderFavories = try encoder.encode(favorites)
+            defaults.set(encoderFavories, forKey: Keys.favorites)
+            return nil
+        } catch {
+            return .unableToFavorite
+        }
+    }
+}
--- iQuote/Utilitis/ErrorMasasge.swift
+++ iQuote/Utilitis/ErrorMasasge.swift
@@ -12,6 +12,8 @@ enum ZenQuoteError : String,Error {
     case unableToCompleted = "Unable to completed your request. Please check your internet connection"
     case inavlidResponse = "Invalid response from server. Please try again."
     case incalidData = "The data received from the server was invalid. Please try again."
+    case unableToFavorite = "There was an error favoriting this Quote. Please try again."
+    case alleredyInFavorites = "You've allredy favorited this Quote. You must very like it!"
 }
 
 enum WikiError : String, Error {
