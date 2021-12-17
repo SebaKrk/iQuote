bc5adf73e0ce9ff9aec11d272c2b658a48a99521
SebaKrk <s.sciuba@gmail.com>
2021-12-17 08:24:48 +0100

    create ConstantSettings and SettingsModel

---
+++ iQuote/Model/SettingsModel.swift
@@ -0,0 +1,50 @@
+//
+//  SettingsModel.swift
+//  iQuote
+//
+//  Created by Sebastian Sciuba on 17/12/2021.
+//
+
+import Foundation
+import UIKit
+
+struct SettingsModel {
+    let title : String
+    let option : [Options]
+}
+
+struct Options {
+    let icon: UIImage?
+    let title: String
+}
+
+
+struct IconImages {
+    static let share = UIImage(systemName: "square.and.arrow.up", withConfiguration: UIImage.SymbolConfiguration(weight: .regular))
+    static let review = UIImage(systemName: "pencil.and.outline", withConfiguration: UIImage.SymbolConfiguration(weight: .regular))
+    static let website = UIImage(systemName: "network", withConfiguration: UIImage.SymbolConfiguration(weight: .regular))
+    static let sugestFeature = UIImage(systemName: "star.circle",withConfiguration: UIImage.SymbolConfiguration(weight: .regular))
+    static let bug = UIImage(systemName: "ladybug.fill",withConfiguration: UIImage.SymbolConfiguration(weight: .regular))
+    static let privacyPolicy = UIImage(systemName: "doc.text",withConfiguration: UIImage.SymbolConfiguration(weight: .regular))
+    static let appSettings = UIImage(systemName: "gear",withConfiguration: UIImage.SymbolConfiguration(weight: .regular))
+    static let appVersion = UIImage(systemName: "info.circle",withConfiguration: UIImage.SymbolConfiguration(weight: .regular))
+}
+
+func configureSettings() -> [SettingsModel] {
+    
+    let modelOne = SettingsModel(title: "", option: [
+        Options(icon: IconImages.share, title: "Share"),
+        Options(icon: IconImages.review , title: "Write a Review")])
+    
+    let modelTwo = SettingsModel(title: "SOURCE", option: [
+        Options(icon: IconImages.website, title:  "Website" ),
+        Options(icon:IconImages.sugestFeature,title: "Sugest a Feature"),
+        Options(icon: IconImages.bug , title: "Report a Bug")])
+    
+    let modelThree = SettingsModel(title: "INFO", option: [
+        Options(icon: IconImages.privacyPolicy , title: "Privacy Policy"),
+        Options(icon: IconImages.appSettings, title: "App Settings"),
+        Options(icon: IconImages.appVersion, title: "App Version")])
+    
+    return [modelOne, modelTwo, modelThree]
+}
---
+++ iQuote/Utilitis/ConstantsSettings.swift
@@ -0,0 +1,36 @@
+//
+//  ConstantsSettings.swift
+//  iQuote
+//
+//  Created by Sebastian Sciuba on 17/12/2021.
+//
+
+import Foundation
+
+struct SettingsTitle {
+    
+    static let share = "Share"
+    static let review = "Write a Review"
+    static let website = "Website"
+    static let sugest = "Sugest a Feature"
+    static let bug = "Report a Bug"
+    static let privacyPolicy = "Privacy Policy"
+    static let appSettings = "App Settings"
+    static let appVersion = "App Version"
+    
+}
+
+enum SettingsMessage : String {
+    
+    case share = "Hi, this is super Quote App , I want to shere with you ..."
+    case sugest = "Hi Sebastian and Mati,\nI just want to sugest ..."
+    case bug =  "Hi Sebastian and Mati,\nI just want to report the bug ..."
+}
+
+enum SettingsURL : String {
+    case share = "https://github.com/SebaKrk/ZenQuotesApp"
+    case review = "https://github.com"
+    case website = "https://github.com/SebaKrk"
+    case privacyPolicy = "https://premium.zenquotes.io/privacy-policy/"
+    case safai = "https://www.google.com"
+}
