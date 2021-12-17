79047810dbb1b508a3b34f46263c37a46d3d2d6a
SebaKrk <s.sciuba@gmail.com>
2021-12-17 08:23:22 +0100

    create func showSafariService and showMailMassageUI in extension UIViewController

--- iQuote/Extensions/UIViewController+Ext.swift
+++ iQuote/Extensions/UIViewController+Ext.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import UIKit
+import SafariServices
+import MessageUI
 
 extension UIViewController {
     
@@ -40,6 +42,52 @@ extension UIViewController {
         swipeDown.direction = .down
         container.addGestureRecognizer(swipeDown)
     }
+    //    MARK: - SafariService
     
+    func showSafariService(with urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        let safariVC = SFSafariViewController(url: url)
+        //        safariVC.preferredBarTintColor = .black
+        safariVC.preferredControlTintColor = .black
+        present(safariVC, animated: true, completion: nil)
+    }
+    //    MARK: - MessageUI
+    
+    func showMailMassageUI (subject: String, message: String, url: String) {
+        if MFMailComposeViewController.canSendMail() {
+            let mailVC = MFMailComposeViewController()
+            mailVC.mailComposeDelegate = self
+            mailVC.setSubject(subject)
+            mailVC.setToRecipients(["s.sciuba@icloud.com","mateuszhalczak@gmail.com"])
+            mailVC.setMessageBody(message, isHTML: false)
+            present(mailVC, animated: true, completion: nil)
+        } else {
+            showSafariService(with: url)
+        }
+    }
+}
+// MARK: - MFMailComposeViewControllerDelegate
+
+extension UIViewController : MFMailComposeViewControllerDelegate {
+    public func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        // handle error and action mesage when create alert vc
+        if let _ = error {
+            controller.dismiss(animated: true, completion: nil)
+            return
+        }
+        switch result {
+        case .cancelled:
+            break
+        case .failed:
+            break
+        case .saved:
+            break
+        case .sent:
+            break
+        @unknown default:
+            fatalError()
+        }
+        controller.dismiss(animated: true, completion: nil)
+    }
 }
 
