4d4b743a4dcc6549e330fbbc7a543acd18c88fa8
SebaKrk <s.sciuba@gmail.com>
2022-01-09 14:40:05 +0100

    change the Unsplash button in PhotoLabViewController

--- iQuote/View/ViewControllers/ExportVC/ImageConfiguration/PhotoLabViewController.swift
+++ iQuote/View/ViewControllers/ExportVC/ImageConfiguration/PhotoLabViewController.swift
@@ -15,7 +15,7 @@ class PhotoLabViewController : UIViewController {
     
     var stackView = UIStackView()
     
-    let unsplashButton = CostumTransButton(imageOne: "BackgroundIcon", imageTwo: "BackgroundIcon")
+    let unsplashButton = CostumTransButton(imageOne: "UnsplashImage1", imageTwo: "UnsplashImage2")
     let photoLibryButton = CostumTransButton(imageOne: "BackgroundIcon", imageTwo: "BackgroundIcon2")
     
     override func viewDidLoad() {
