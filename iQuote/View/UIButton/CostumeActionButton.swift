1e1e678693ef9a5893f61916519a320d458f4d25
SebaKrk <s.sciuba@gmail.com>
2022-01-13 11:04:51 +0100

    create CostumeActionButton

---
+++ iQuote/View/UIButton/CostumeActionButton.swift
@@ -0,0 +1,34 @@
+//
+//  CostumeActionButton.swift
+//  iQuote
+//
+//  Created by Sebastian Sciuba on 13/01/2022.
+//
+
+
+import UIKit
+
+class CostumeActionButton : UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    init(name: String ) {
+        super.init(frame: .zero)
+        setTitle(name, for: .normal)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .black
+        setTitleColor(.white, for: .normal)
+        titleLabel?.font = UIFont(name: "Roboto-Regular", size: 14)
+    }
+}
