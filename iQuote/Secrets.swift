9aeab0ab4ba1954df96b6a8d5e4b60aaf12be7b2
SebaKrk <s.sciuba@gmail.com>
2022-05-02 08:58:05 +0200

    test 2

---
+++ iQuote/Secrets.swift
@@ -0,0 +1,17 @@
+//
+//  Secrets.swift
+//  iQuote
+//
+//  Created by Sebastian Sciuba on 02/05/2022.
+//
+
+
+import Foundation
+
+enum Secrets {
+    
+    static let appVersion = "0.1.5"
+    static let apiKey = "321803f7a30a90c98bd64c82dd1de4faf055834a"
+    static let accessKey = "an5tkxA_i2xrre0psghsbWkntbm-d0QvBXoLfcAH7IY"
+}
+
