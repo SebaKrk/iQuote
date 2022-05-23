94dd199e8c56eb69fd7811654ece8ab1d51eb768
SebaKrk <s.sciuba@gmail.com>
2022-05-23 22:02:40 +0200

    create Onboarding file

---
+++ iQuote/View/ViewControllers/Onbording/OnboardingStart.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingStart.swift
+//  iQuote
+//
+//  Created by Sebastian Sciuba on 23/05/2022.
+//
+
+import Foundation
+import UIKit
+
+class OnboardingStart: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
