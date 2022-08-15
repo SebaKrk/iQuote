0643aabf20663f82dd831eafe799ea3482e170dc
SebaKrk <s.sciuba@gmail.com>
2022-08-15 07:57:28 +0200

    [WIP] Onboarding

---
+++ iQuote/Controller/Onboarding/OnboardingVC.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingVC.swift
+//  iQuote
+//
+//  Created by Sebastian Sciuba on 15/08/2022.
+//
+
+import SwiftUI
+
+struct OnboardingVC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingVC_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingVC()
+    }
+}
